f6a759c15942c5975366d223f18120629140e925
George Louthan <duplico@dupli.co>
2022-07-19 21:30:44 -0500

    Make duration ticks more explicitly named.

--- ccs_workspace/qc2022/leds.h
+++ ccs_workspace/qc2022/leds.h
@@ -11,7 +11,7 @@
 #include <stdint.h>
 
 #define LED_COUNT 4
-#define LEGS_DUR_STEP 14
+#define TICKS_PER_LED_ANIM_DUR 14 // Number of system ticks per unit of duration in animations.
 
 #define BAND_TWINKLE_STEPS_FAST 100
 #define BAND_TWINKLE_STEPS_SLOW 400
