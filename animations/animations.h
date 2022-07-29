49a6880cee832aefdd778c7cec6b7bcc3043b296
George Louthan <duplico@dupli.co>
2022-07-29 01:20:24 -0500

    Add base animation for connection count.

--- animations/animations.h
+++ animations/animations.h
@@ -15,6 +15,10 @@ extern const rgbcolor_t h02_frames[][5];
 extern const uint16_t h02_durations[];
 extern const uint16_t h02_fade_durs[];
 extern const leds_animation_t h02;
+extern const rgbcolor_t meta_connects_frames[][5];
+extern const uint16_t meta_connects_durations[];
+extern const uint16_t meta_connects_fade_durs[];
+extern const leds_animation_t meta_connects;
 extern const rgbcolor_t meta_newpair_frames[][5];
 extern const uint16_t meta_newpair_durations[];
 extern const uint16_t meta_newpair_fade_durs[];
@@ -52,21 +56,22 @@ extern const uint16_t meta_z_brightness2_durations[];
 extern const uint16_t meta_z_brightness2_fade_durs[];
 extern const leds_animation_t meta_z_brightness2;
 #define ANIM_COUNT 3
-#define ANIM_COUNT_INCL_META 12
+#define ANIM_COUNT_INCL_META 13
 #define ANIM_H00 0
 #define ANIM_H01 1
 #define ANIM_H02 2
-#define ANIM_META_NEWPAIR 3
-#define ANIM_META_NEWUBER 4
-#define ANIM_META_PAIR 5
-#define ANIM_META_PAIRUBER 6
-#define ANIM_META_STARTUP_FADE 7
-#define ANIM_META_STARTUP_SPIN 8
-#define ANIM_META_Z_BRIGHTNESS0 9
-#define ANIM_META_Z_BRIGHTNESS1 10
-#define ANIM_META_Z_BRIGHTNESS2 11
+#define ANIM_META_CONNECTS 3
+#define ANIM_META_NEWPAIR 4
+#define ANIM_META_NEWUBER 5
+#define ANIM_META_PAIR 6
+#define ANIM_META_PAIRUBER 7
+#define ANIM_META_STARTUP_FADE 8
+#define ANIM_META_STARTUP_SPIN 9
+#define ANIM_META_Z_BRIGHTNESS0 10
+#define ANIM_META_Z_BRIGHTNESS1 11
+#define ANIM_META_Z_BRIGHTNESS2 12
 #define ANIM_TYPE_SOLID 0
 #define ANIM_TYPE_FASTTWINKLE 1
 #define ANIM_TYPE_SLOWTWINKLE 2
-extern const leds_animation_t *all_anims[12];
+extern const leds_animation_t *all_anims[13];
 #endif // _H_
