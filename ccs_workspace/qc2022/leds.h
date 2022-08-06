168f9942ae2e6349546d45094c1c7968e8c5feb5
George Louthan <duplico@dupli.co>
2022-08-05 19:32:50 -0500

    Add doxygen docs to LED module

--- ccs_workspace/qc2022/leds.h
+++ ccs_workspace/qc2022/leds.h
@@ -1,8 +1,10 @@
-/*
- * leds.h
- *
- *  Created on: Oct 17, 2019
- *      Author: george
+/// Header for the high-level LED animation driver.
+/**
+ **
+ ** \file leds.h
+ ** \author George Louthan
+ ** \date   2022
+ ** \copyright (c) 2015-2022 George Louthan @duplico. MIT License.
  */
 
 #ifndef LEDS_H_
@@ -10,35 +12,48 @@
 
 #include <stdint.h>
 
+/// Number of RGB LEDs in the system
 #define LED_COUNT 5
-#define TICKS_PER_LED_ANIM_DUR 10 // Number of system ticks per unit of duration in animations.
+/// Conversion factor from animation duration unit to system ticks.
+/**
+ ** This is used by dividing the animation duration by this value. So,
+ ** if animation duration units are in milliseconds, then set this to
+ ** 10.
+ */
+#define ANIM_DUR_UNIT_PER_SYSTEM_TICK 10
 
+/// System ticks between fast twinkles.
 #define LEDS_TWINKLE_STEPS_FAST 30
+/// System ticks between slow twinkles.
 #define LEDS_TWINKLE_STEPS_SLOW 125
-#define LEDS_TWINKLE_STEPS_HARD 14
 
+/// Maximum number of non-ambient animations that can be queued.
 #define LEDS_QUEUE_MAXLEN 5
+/// Value for an animation ID indicating there is no animation selected.
 #define LEDS_ID_NO_ANIM 255
 
+/// Collection of RGB colors of 16 unsigned bits per channel.
 typedef struct {
     uint16_t red;
     uint16_t green;
     uint16_t blue;
 } rgbcolor16_t;
 
-
+/// Collection of RGB colors of 8 unsigned bits per channel.
 typedef struct {
     uint8_t red;
     uint8_t green;
     uint8_t blue;
 } rgbcolor_t;
 
+/// Collection of signed 32-bit RGB channels used for color differences/steps.
 typedef struct {
     int_fast32_t red;
     int_fast32_t green;
     int_fast32_t blue;
 } rgbdelta_t;
 
+/// An LED animation!
 typedef struct {
     const rgbcolor_t (*colors)[LED_COUNT];
     const uint16_t *durations;
